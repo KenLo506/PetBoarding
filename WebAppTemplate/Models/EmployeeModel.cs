002eb59dace2ec362908fa301f904e0a3b75e666
Ken Lo <loken506@gmail.com>
2025-05-05 20:25:50 -0400

    Add database

---
+++ WebAppTemplate/Models/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public Guid EmployeeID { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public DateTime ShiftCheckIn { get; set; }
+        public DateTime ShiftCheckOut { get; set; }
+        [Required]
+        public FacilityModel FacilityID { get; set; }
+        public EmployeeModel()
+        {
+            EmployeeID = Guid.NewGuid();
+        }
+    }
+}
---
+++ WebAppTemplate/Models/FacilityModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.Models
+{
+    public class FacilityModel
+    {
+        [Key]
+        public Guid FacilityID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FacilityName { get; set; }
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        public int MaxOccupancy { get; set; }
+
+        public List<BookingModel> Bookings { get; set; }
+
+        public FacilityModel()
+        {
+            FacilityID = Guid.NewGuid();
+            Bookings = new List<BookingModel>();
+        }
+    }
+}
