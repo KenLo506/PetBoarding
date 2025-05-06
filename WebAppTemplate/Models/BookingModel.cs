806c64cbab685364023d7d3bc415d329304e9db7
Ken Lo <loken506@gmail.com>
2025-05-06 00:44:45 +0000

    AB#1118 - Add Database (#2)
    Reviewed-on: https://git.websurfingstudios.com/Phase1/PetBoarding_KenLo/pulls/2
    Reviewed-by: Ryan Muller <ryanmuller@websurfingstudios.com>

---
+++ WebAppTemplate/Models/BookingModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.Models
+{
+    public enum BookingStatus
+    {
+        Scheduled,
+        CheckedIn,
+        CheckedOut,
+        Canceled
+    }
+    public class BookingModel
+    {
+        [Key]
+        public Guid BookingID { get; set; }
+        [Required]
+        public PetModel PetID { get; set; }
+
+        public string PetInstructions { get; set; }
+
+        public DateTime BookingStartTime { get; set; }
+
+        public DateTime BookingEndTime { get; set; }
+
+        public DateTime? ActualCheckInTime { get; set; }
+
+        public DateTime? ActualCheckOutTime { get; set; }
+
+        public EmployeeModel CheckInByEmployeeID { get; set; } 
+
+        public EmployeeModel CheckedOutByEmployeeID { get; set; } 
+
+        public FacilityModel FacilityID { get; set; } 
+
+        public BookingStatus Status { get; set; }
+
+        public string FeedingSchedule { get; set; }
+
+        public string FoodAmount { get; set; }
+
+        public BookingModel()
+        {
+            BookingID = Guid.NewGuid();
+            Status = BookingStatus.Scheduled;
+        }
+
+    }
+}
---
+++ WebAppTemplate/Models/PetModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.Models
+{
+    public class PetModel
+    {
+        [Key]
+        public Guid PetID { get; set; }
+        [Required]
+        public PetOwnerModel OwnerID { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public int Age { get; set; }
+
+        public string Vaccines { get; set; }
+
+        public string Medications { get; set; }
+
+        public string ExtraNotes { get; set; }
+
+        public List<BookingModel> Bookings { get; set; }
+        public PetModel()
+        {
+            PetID = Guid.NewGuid();
+            Bookings = new List<BookingModel>();
+        }
+
+    }
+}
