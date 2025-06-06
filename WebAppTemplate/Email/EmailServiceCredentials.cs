97e0402654083f05c9ba65ea3ec7d443d6ff6753
Ken Lo <loken506@gmail.com>
2025-06-06 19:41:29 -0400

    Add email confirmation

---
+++ WebAppTemplate/Email/EmailServiceCredentials.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Net.Mail;
+using System.Net;
+
+ public static class EmailServiceCredentials
+{
+    public static string EmailSMTPUrl { get; private set; }
+    public static string PortNumber { get; private set; }
+    public static string EmailSMTPUserNameHash { get; private set; }
+    public static string EmailSMTPPasswordHash { get; private set; }
+    public static string EmailFromAddress { get; private set; }
+    public static string EmailFromName { get; private set; }
+    public static string EmailAppName { get; private set; }
+
+    public static void SetCredentials(string emailSMTPUrl, string portNumber, string emailSMTPUserNameHash, string emailSMTPPasswordHash, string emailFromAddress, string emailFromName, string emailAppName)
+    {
+        EmailSMTPUrl = emailSMTPUrl;
+        PortNumber = portNumber;
+        EmailSMTPPasswordHash = emailSMTPPasswordHash;
+        EmailSMTPUserNameHash = emailSMTPUserNameHash;
+        EmailFromAddress = emailFromAddress;
+        EmailFromName = emailFromName;
+        EmailAppName = emailAppName;
+
+
+    }
+
+    //Call from global application
+    public static void PopulateEmailCredentialsFromAppConfig()
+    {
+        string emailSMTPURL = ConfigurationManager.AppSettings["emailSMTPURL"].ToString();
+        string portNumber = ConfigurationManager.AppSettings["portNumber"].ToString();
+        string emailSMTPUserNameHash = ConfigurationManager.AppSettings["emailSMTPUserNameHash"].ToString();
+        string emailSMTPPasswordHash = ConfigurationManager.AppSettings["emailSMTPPasswordHash"].ToString();
+        string emailFromAddress = ConfigurationManager.AppSettings["emailFromAddress"].ToString();
+        string emailFromName = ConfigurationManager.AppSettings["emailFromName"].ToString();
+        string emailAppName = ConfigurationManager.AppSettings["emailAppName"].ToString();
+
+        SetCredentials(emailSMTPURL, PortNumber, emailSMTPUserNameHash, emailSMTPPasswordHash, emailFromAddress, emailFromName, emailAppName);
+    }
+
+}
---
+++ WebAppTemplate/Helpers/EmailHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+
+namespace WebAppTemplate.Helpers
+{   public static class EmailHelpers
+    {
+        public static SmtpClient GetSmtpClient()
+        {
+            SmtpClient smtpClient = new SmtpClient(EmailServiceCredentials.EmailSMTPUrl);
+            smtpClient.Port = int.Parse(EmailServiceCredentials.PortNumber);
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential(
+                EmailServiceCredentials.EmailSMTPUserNameHash,
+                EmailServiceCredentials.EmailSMTPPasswordHash
+            );
+
+            return smtpClient;
+        }
+
+        public static MailMessage GenerateMailMessage(string destination, string subject, string body)
+        {
+            MailMessage mailMessage = new MailMessage(
+                new MailAddress(EmailServiceCredentials.EmailFromAddress, EmailServiceCredentials.EmailFromName),
+                new MailAddress(destination)
+            );
+            mailMessage.Subject = EmailServiceCredentials.EmailAppName + " - " + subject;
+            mailMessage.Body = body;
+            mailMessage.IsBodyHtml = true;
+
+            return mailMessage;
+        }
+    }
+}
