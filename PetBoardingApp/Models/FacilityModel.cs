a4ba530f94aac5066e313dc34a01d7b817af459c
Ken Lo <loken506@gmail.com>
2025-06-09 23:48:36 -0400

    Complete Registration

---
+++ PetBoardingApp/Models/FacilityModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetBoardingApp.Models
+{
+    public class FacilityModel
+    {
+        [Key]
+        public Guid FacilityID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FacilityName { get; set; }
+        [MaxLength(100)]
+        public string Address { get; set; }
+        public int MaxOccupancy { get; set; }
+
+        public List<BookingModel> Bookings { get; set; }
+
+        public List<EmployeeModel> Employees { get; set; }
+
+        public FacilityModel()
+        {
+            FacilityID = Guid.NewGuid();
+            Bookings = new List<BookingModel>();
+        }
+    }
+}
---
+++ PetBoardingApp/Models/PetOwnerModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PetBoardingApp.Models
+{
+    public class PetOwnerModel
+    {
+        [Key]
+        public Guid OwnerID { get; set; } // PK
+        [Required]
+        public string FullName { get; set; }
+        [MaxLength(1000)]
+        public string Email { get; set; }
+        [MaxLength(10)]
+        public string Phone { get; set; }
+        [MaxLength(10)]
+        public string EmergencyContact { get; set; }
+
+        public List<PetModel> Pets { get; set; }
+
+        public PetOwnerModel()
+        {
+            OwnerID = Guid.NewGuid();
+            Pets = new List<PetModel>();    
+        }
+
+    }
+}
